0014200fb9542b1ef00f4d44cb2d4462363ef9a4
Shareen Ali <ali.shareen19@gmail.com>
2018-11-24 23:03:49 -0500

    Removed unnecessary files

--- src/course/CourseInterface.java
+++ src/course/CourseInterface.java
@@ -1,12 +1,8 @@
 package course;
 
-import schema.CourseData;
-
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import java.util.HashMap;
-import java.util.List;
 
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
